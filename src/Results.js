a218ebffa4243a612282bb721581d8c1f63796c0
Renoj Varghese <renoj.varghese@uconn.edu>
2018-05-14 22:26:58 -0400

    setting up results page

--- src/Results.js
+++ src/Results.js
@@ -4,6 +4,13 @@ export class Results extends Component {
     constructor(props) {
         super(props)
     }
+    get results() {
+        const officials = this.props.data.officials;
+        const offices = this.props.data.offices;
+        return offices.map(office => {
+            
+        })
+    }
     render() {
 
     }
