8e3f16962b6f5cc8231c2bd37a1ba728e0c1ebd8
Renoj Varghese <renoj.varghese@uconn.edu>
2018-05-18 20:57:06 -0400

    trying to get dev server running.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-const htmlPlugin = new HtmlWebPackPlugin({
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
   template: "./src/index.html",
   filename: "./index.html"
 });
@@ -15,8 +15,26 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
       }
     ]
   },
-  plugins: [htmlPlugin]
+  plugins: [htmlWebpackPlugin]
 };
