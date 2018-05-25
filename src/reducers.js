d70c2cd6afba41f821fe9ca5b847461d991a6faa
Renoj Varghese <renoj.varghese@uconn.edu>
2018-05-24 21:29:53 -0400

    export store

--- src/reducers.js
+++ src/reducers.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const reducer = (state = {}, action) => {
   switch (action.type) {
     case 'NEW_STATE':
@@ -19,3 +21,5 @@ const createOfficerArray = data => {
     });
   });
 };
+
+export const store = createStore(reducer);
