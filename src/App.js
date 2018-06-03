5398ac67407c28ed7d5e80b55a0250df6c489d78
Renoj Varghese <renoj.varghese@uconn.edu>
2018-06-02 21:37:51 -0400

    header text

--- src/App.js
+++ src/App.js
@@ -37,9 +37,12 @@ class App extends Component {
     return (
       <div className="app">
         <header className="app-header">
-          <h1 className="app-title">App Name TBD</h1>
+          <h1 className="app-title">Government Contact</h1>
 
-          <p className="app-desc">app desc TBD</p>
+          <p className="app-desc">
+            Search for the people who serve you. Take action, and contact them
+            over issues you care about
+          </p>
           <div className="img-container logo-img">
             <img className="pop-up" src="img/logo.svg" alt="gov-contact logo" />
           </div>
