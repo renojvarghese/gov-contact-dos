85ba9e2779cf9115fb39c2f8e4207349f27b1bc6
Renoj Varghese <renoj.varghese@uconn.edu>
2018-07-04 18:45:36 -0400

    Adding social media links

--- src/components/Official.js
+++ src/components/Official.js
@@ -1,67 +1,92 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
+const socialLinks = {
+    Twitter: "https://www.twitter.com/@",
+    Facebook: "https://www.facebook.com/",
+    Instagram: "https://www.instagram.com/@",
+    YouTube: "https://www.youtube.com/"
+};
 export class Official extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { imageStatus: 'loading' };
-  }
+    constructor(props) {
+        super(props);
+        this.state = { imageStatus: "loading" };
+    }
 
-  handleImageLoaded() {
-    this.setState({ imageStatus: 'loaded' });
-  }
+    handleImageLoaded() {
+        this.setState({ imageStatus: "loaded" });
+    }
 
-  handleImageErrored() {
-    this.setState({ imageStatus: 'failed to load' });
-  }
-  render() {
-    const official = this.props.official;
-    return (
-      <div
-        className={
-          'card-container pop-up ' +
-          (official.party === 'Republican'
-            ? 'republican'
-            : official.party === 'Democratic'
-              ? 'democrat'
-              : '')
-        }
-      >
-        <div className="portrait-container">
-          <div className={'img-container ' + this.state.imageStatus}>
-            <img
-              src={this.props.official.photoUrl}
-              onLoad={this.handleImageLoaded.bind(this)}
-              onError={this.handleImageErrored.bind(this)}
-            />
-          </div>
-        </div>
-        <div className="name">{this.props.official.name}</div>
-        <div className="office-name">{this.props.official.officeName}</div>
-        {this.props.official.emails
-          ? this.props.official.emails.map((email, i) => {
-              return (
-                <div className="email" key={'email_' + i}>
-                  <a className="email-link" href={'mailto:' + email}>
-                    {email}
-                  </a>
+    handleImageErrored() {
+        this.setState({ imageStatus: "failed to load" });
+    }
+    render() {
+        const official = this.props.official;
+        return (
+            <div
+                className={
+                    "card-container pop-up " +
+                    (official.party ? official.party.toLowerCase() : "")
+                }
+            >
+                <div className="portrait-container">
+                    <div className={"img-container " + this.state.imageStatus}>
+                        <img
+                            src={this.props.official.photoUrl}
+                            onLoad={this.handleImageLoaded.bind(this)}
+                            onError={this.handleImageErrored.bind(this)}
+                        />
+                    </div>
                 </div>
-              );
-            })
-          : ''}
-        {this.props.official.phones
-          ? this.props.official.phones.map((phone, i) => {
-              return (
-                <div className="phone" key={'phone_' + i}>
-                  <span className="phone-link">{phone}</span>
-                </div>
-              );
-            })
-          : ''}
-      </div>
-    );
-  }
+                <div className="name">{official.name}</div>
+                <div className="office-name">{official.officeName}</div>
+                {official.emails
+                    ? official.emails.map((email, i) => {
+                          return (
+                              <div className="email" key={"email_" + i}>
+                                  <a
+                                      className="email-link"
+                                      href={"mailto:" + email}
+                                  >
+                                      {email}
+                                  </a>
+                              </div>
+                          );
+                      })
+                    : ""}
+                {official.phones
+                    ? official.phones.map((phone, i) => {
+                          return (
+                              <div className="phone" key={"phone_" + i}>
+                                  <span className="phone-link">{phone}</span>
+                              </div>
+                          );
+                      })
+                    : ""}
+                {official.channels
+                    ? official.channels.map((channel, i) => {
+                          const suffix = channel.type;
+                          return (
+                              <div
+                                  className={"social " + suffix}
+                                  key={"social_" + suffix + i}
+                              >
+                                  <a
+                                      className={"social-link " + suffix}
+                                      href={
+                                          socialLinks[channel.type] + channel.id
+                                      }
+                                  >
+                                      {"@" + channel.id}
+                                  </a>
+                              </div>
+                          );
+                      })
+                    : ""}
+            </div>
+        );
+    }
 }
 Official.propTypes = {
-  official: PropTypes.object.isRequired
+    official: PropTypes.object.isRequired
 };
