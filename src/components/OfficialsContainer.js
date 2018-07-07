c05cca106ad7f98a30cd4f74a09dfb53c0192db3
Renoj Varghese <renoj.varghese@uconn.edu>
2018-07-07 15:19:29 -0400

    Connecting OfficialsContainer to Redux store

--- src/components/OfficialsContainer.js
+++ src/components/OfficialsContainer.js
@@ -1,30 +1,21 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Official } from "./Official";
-
-export class OfficialsContainer extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    get officialData() {
-        return this.context.store
-            .getState()
-            .officials.map((official, index) => {
-                return (
-                    <Official key={"official_" + index} official={official} />
-                );
-            });
-    }
-    render() {
-        return (
-            <div className="officials-container" id="officials-results">
-                {this.officialData}
-            </div>
-        );
-    }
-}
-
-OfficialsContainer.contextTypes = {
-    store: PropTypes.object
+import { connect } from "react-redux";
+const OfficialsContainer = ({ officials }) => {
+    return (
+        <div className="officials-container" id="officials-results">
+            {officials.map((official, i) => {
+                return <Official official={official} key={"official_" + i} />;
+            })}
+        </div>
+    );
 };
+
+const mapStateToProps = state => {
+    return {
+        officials: state.officials
+    };
+};
+
+export default connect(mapStateToProps)(OfficialsContainer);
