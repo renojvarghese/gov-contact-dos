8b7e99e7d245afe0d6db331f6fc8cb86ccff6dd8
Renoj Varghese <renoj.varghese@uconn.edu>
2018-07-07 15:19:56 -0400

    connecting App.js to Redux store

--- src/App.js
+++ src/App.js
@@ -1,48 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
-import { SearchFormContainer } from "./components/SearchFormContainer";
-import { OfficialsContainer } from "./components/OfficialsContainer";
+import SearchFormContainer from "./components/SearchFormContainer";
+import OfficialsContainer from "./components/OfficialsContainer";
 import { Header } from "./components/Header";
 import { PropTypes } from "prop-types";
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.getData = this.getData.bind(this);
-        this.state = {
-            res: null,
-            error: false
-        };
-    }
-    getData(res) {
-        if (res.error) {
-            this.context.store.dispatch({
-                type: "ERROR",
-                data: res
-            });
-            return this.setState({
-                error: true
-            });
-        }
-        this.context.store.dispatch({
-            type: "NEW_STATE",
-            data: res
-        });
-        this.setState({
-            data: true
-        });
-    }
     render() {
-        const data = this.state.res;
         return (
             <div className="app">
                 <Header />
-                <SearchFormContainer handleData={this.getData} />
-
-                {this.state.data ? (
-                    <OfficialsContainer store={this.props.store} />
-                ) : (
-                    ""
-                )}
+                <SearchFormContainer />
+                <OfficialsContainer />
             </div>
         );
     }
