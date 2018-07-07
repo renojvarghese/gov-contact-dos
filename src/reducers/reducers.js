d04792a9f9a855a9f4bdf403a9092ac3441a0d0f
Renoj Varghese <renoj.varghese@uconn.edu>
2018-07-07 15:19:06 -0400

    updating default state of reducer

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,32 +1,35 @@
-export const reducer = (state = { error: false }, action) => {
-  switch (action.type) {
-    case 'NEW_STATE':
-      return {
-        officials: createOfficerArray(action.data),
-        error: false
-      };
-    case 'ERROR':
-      return {
-        officials: [],
-        error: true
-      };
-    default:
-      return state;
-  }
+export const reducer = (
+    state = { error: false, officials: [] },
+    action = {}
+) => {
+    switch (action.type) {
+        case "NEW_OFFICIALS":
+            return {
+                officials: createOfficerArray(action.data),
+                error: false
+            };
+        case "ERROR":
+            return {
+                officials: [],
+                error: true
+            };
+        default:
+            return state;
+    }
 };
 
 const createOfficerArray = data => {
-  let offices = data.offices;
-  if (!offices) return {};
-  let officials = [];
-  offices.map(office => {
-    return office.officialIndices.map(officialIndex => {
-      let myofficial = {
-        ...data.officials[officialIndex],
-        officeName: office.name
-      };
-      officials.push(myofficial);
+    let offices = data.offices;
+    if (!offices) return {};
+    let officials = [];
+    offices.map(office => {
+        return office.officialIndices.map(officialIndex => {
+            let myofficial = {
+                ...data.officials[officialIndex],
+                officeName: office.name
+            };
+            officials.push(myofficial);
+        });
     });
-  });
-  return officials.slice(2);
+    return officials.slice(2);
 };
